f3669104c948ca48f8bd1f0c3a92f75b0d1de5e8
Bishal Sapkota <bishalspkt@gmail.com>
2024-01-08 01:30:03 +1100

    feat: update map label

--- src/components/map-controls/map-controls.tsx
+++ src/components/map-controls/map-controls.tsx
@@ -2,36 +2,28 @@ import { useState } from "react";
 import UploadGeoJSONButton from "./upload-geojson.js";
 import PasteGeoJSONForm from "./paste-geojson.js";
 import { Button } from "../ui/button.js";
-import { Download, Layers, Upload } from "lucide-react";
+import { Download, Import, Layers, X } from "lucide-react";
 
 interface UploadGeoJSONButtonProps {
     setGeoJSON: React.Dispatch<React.SetStateAction<Record<string, unknown> | undefined>> // Update the type of geoJSON
 }
 
 type PanelType = "upload" | "layers";
-type PanelStatus = "minimized" | "maximized" | "hidden";
-type PanelAction = "toggle-hidden" | "toggle-maximized";
+type PanelStatus = "maximized" | "hidden";
 
 export default function MapControls({ setGeoJSON }: UploadGeoJSONButtonProps) {
     const [uploadPanelStatus, setUploadPanelStatus] = useState<PanelStatus>("maximized");
     const [layersPanelStatus, setLayersPanelStatus] = useState<PanelStatus>("hidden");
 
-
-    const togglePanel = (panel: PanelType, action: PanelAction) => {
+    const togglePanel = (panel: PanelType) => {
         switch (panel) {
             case "upload":
                 setLayersPanelStatus("hidden")
-
-                action === "toggle-hidden" 
-                    ? setUploadPanelStatus(uploadPanelStatus === "hidden" ? "maximized" : "hidden") 
-                    : setUploadPanelStatus(uploadPanelStatus === "minimized" ? "maximized" : "minimized");
+                uploadPanelStatus === "hidden" ? setUploadPanelStatus("maximized") : setUploadPanelStatus("hidden")
                 break;
             case "layers":
                 setUploadPanelStatus("hidden")
-
-                action === "toggle-hidden"
-                    ? setLayersPanelStatus(layersPanelStatus === "hidden" ? "maximized" : "hidden")
-                    : setLayersPanelStatus(layersPanelStatus === "minimized" ? "maximized" : "minimized");
+                layersPanelStatus === "hidden" ? setLayersPanelStatus("maximized") : setLayersPanelStatus("hidden")
                 break;
             default:
                 alert("Something unexpected happened. Panel has unknown value")
@@ -40,16 +32,18 @@ export default function MapControls({ setGeoJSON }: UploadGeoJSONButtonProps) {
 
     return (
         <>
-            <div className="fixed top-16 left-4 flex flex-col gap-2">
-                <Button variant="outline" size="icon" className="rounded-3xl" onClick={() => togglePanel("upload", "toggle-hidden")}><Upload className="h-5 w-5" /></Button>
-                <Button variant="outline" size="icon" className="rounded-3xl" onClick={() => togglePanel("layers", "toggle-hidden")}><Layers className="h-5 w-5" /></Button>
+            <div className="fixed bottom-4 left-4 flex flex gap-2">
+                <Button variant="outline" size="icon" className="rounded-3xl" onClick={() => togglePanel("upload")}><Import className="h-5 w-5" /></Button>
+                <Button variant="outline" size="icon" className="rounded-3xl" onClick={() => togglePanel("layers")}><Layers className="h-5 w-5" /></Button>
                 <Button variant="outline" size="icon" className="rounded-3xl"><Download className="h-5 w-5" /></Button>
             </div>
 
             {uploadPanelStatus !== "hidden" &&
-                <div className="fixed left-8 bottom-0 mx-8 rounded sm:w-[450px] w-[300px] text-black font-semibold bg-white shadow-lg border border-b-0">
-                    <div className="flex border-b border-gray-300 px-4 py-2 my-2 gap-2 hover:cursor-pointer" onClick={() => togglePanel("upload", "toggle-maximized")}>
+                <div className="fixed left-4 bottom-20 rounded sm:w-[450px] w-[300px] text-black font-semibold bg-white shadow-lg border border-b-0">
+                    <div className="flex border-b border-gray-300 px-4 py-2 my-2 gap-2 hover:cursor-pointer" onClick={() => togglePanel("upload")}>
+                        <Import className="h-5 w-5" />
                         <p>Import GeoJSON</p>
+                        <X className="h-5 w-5 ml-auto" />
                     </div>
                     {uploadPanelStatus === "maximized" && (
                         <div className="flex flex-col gap-2 px-4 py-2 text-left">
@@ -62,15 +56,19 @@ export default function MapControls({ setGeoJSON }: UploadGeoJSONButtonProps) {
                 </div>
             }
             {layersPanelStatus !== "hidden" &&
-                <div className="fixed left-8 bottom-0 mx-8 rounded sm:w-[450px] w-[300px] text-black font-semibold bg-white shadow-lg border border-b-0">
-                    <div className="flex border-b border-gray-300 px-4 py-2 my-2 gap-2 hover:cursor-pointer">
+                <div className="fixed left-4 bottom-20 rounded sm:w-[450px] w-[300px] text-black font-semibold bg-white shadow-lg border border-b-0">
+                    <div className="flex border-b border-gray-300 px-4 py-2 my-2 gap-2 hover:cursor-pointer" onClick={() => togglePanel("layers")}>
+                        <Layers className="h-5 w-5" />
                         <p>Your Layers</p>
+                        <X className="h-5 w-5 ml-auto" />
                     </div>
                     {layersPanelStatus === "maximized" && (
                         <div className="flex flex-col gap-2 px-4 py-2 text-left">
                             <p>You have not added any layers</p>
                             <p className="text-gray-600 text-sm">To add layers, import your GeoJSON</p>
-                            <Button className="mr-auto" onClick={() => togglePanel("upload", "toggle-hidden")}>Import your GeoJSON</Button>
+                            <div className="py-2 mr-auto">
+                                <Button className="py-2" onClick={() => togglePanel("upload")}>Import your GeoJSON</Button>
+                            </div>
                         </div>
                     )}
                 </div>
--- src/components/map/map-label.tsx
+++ src/components/map/map-label.tsx
@@ -1,5 +1,10 @@
+import { MapPinned } from "lucide-react";
+
 export default function MapLabel() {
     return (
-            <h2 className="fixed top-2 left-2 text-white text-lg text-gray-800 text-shadow">GeoJSON.app</h2>
+        <div className="fixed top-2 left-2 flex gap-1" style={{ textShadow: '1px 1px 2px white' }}>
+            <MapPinned className="h-8 w-8 p-1 text-gray-800" />
+            <h2 className="text-xl text-gray-800 font-bold">geojson.app</h2>
+        </div>
     )
 }
