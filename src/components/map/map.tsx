ed1f169a890aa78f47efba6295b7d9ba182bf863
Bishal Sapkota <bishalspkt@gmail.com>
2024-01-08 16:13:29 +1100

    feat: add a real map

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,8 +1,22 @@
 import { useRef, useEffect, useState, ElementRef } from 'react';
-import maplibregl from 'maplibre-gl';
+import maplibregl, { StyleSpecification } from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import './map.css';
 import { addGeoJSONLayer } from '../../lib/map-utils';
+import layers from 'protomaps-themes-base';
+
+const mapLibreMapStyle: StyleSpecification = {
+    version: 8,
+    glyphs:'https://protomaps.github.io/basemaps-assets/fonts/{fontstack}/{range}.pbf',
+    sources: {
+        "protomaps-mvt": {
+            type: "vector",
+            tiles: ["https://tiles.geojson.app/20240107/{z}/{x}/{y}.mvt"],
+            attribution: '<a href="https://protomaps.com">Protomaps</a> © <a href="https://openstreetmap.org">OpenStreetMap</a>'
+        }
+    },
+    layers: layers("protomaps-mvt","light")
+}
 
 interface MapProps {
     uploadedGeoJSON?: Record<string, unknown>; // Update the type of uploadedGeoJSON
@@ -21,14 +35,17 @@ export default function Map({ uploadedGeoJSON }: MapProps) {
         }
         map.current = new maplibregl.Map({
             container: mapContainer.current!,
-            style: `https://demotiles.maplibre.org/style.json`,
+            style: mapLibreMapStyle,
             center: [0, 0],
             zoom: 2
         });
 
-        map.current.on('load', () => {
-            setMapReady(true);
-        })
+        map.current.on('load', () => { setMapReady(true); });
+
+        return () => {
+            map.current?.remove();
+            map.current = null;
+        }
     }, []);
 
     // Update uploaded GeoJson Layer
